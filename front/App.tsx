eb4d79bf30a37de761468851763a3b1ea1f628cf
chb6734 <chb6734@gmail.com>
2024-04-22 15:22:08 +0900

    feat; [FE] 프로그램 실행시 fetch받아서 랜덤으로 돌리는 기능 추가

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -9,17 +9,76 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import HomeScreen from './app/screens/HomeScreen';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+const headers = {withCredentials: true};
+
+interface QouteProps {
+  contents: string;
+  author: string;
+}
 
 function App(): React.JSX.Element {
-  return <HomeScreen />;
+  const [qoutes, setQoutes] = useState<QouteProps | null>(null);
+  const send_param = {headers};
+  const itemlist: QouteProps[] = [];
+  var itemCnt: number = 0;
+
+  useEffect(() => {
+    fetch('http://10.0.2.2:8080/getlq')
+      .then(res => res.json())
+      .then(data => {
+        if (data) {
+          data.forEach((item: QouteProps) => {
+            itemlist.push(item);
+            console.log('console : ' + itemlist);
+          });
+          console.log('itemlist : ' + itemlist);
+          itemCnt = itemlist.length;
+          setQoutes(itemlist[Math.floor(Math.random() * itemCnt)]);
+        } else {
+          console.log('fail');
+        }
+        // console.log(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  // useEffect(() => {
+  //   const getQoutes = async () => {
+  //     try {
+  //       const res: any = await fetch('http://10.0.2.2:8080/getlq');
+  //       const qouteRes = res.json();
+  //       console.log('res : ' + res.data);
+
+  //       const itemlist: QouteProps[] = [];
+  //       if (qouteRes.data) {
+  //         qouteRes.forEach((item: QouteProps) => {
+  //           itemlist.push(item);
+  //           console.log('console : ' + itemlist);
+  //         });
+  //         setQoutes(itemlist[Math.random()]);
+  //       }
+  //     } catch (err) {
+  //       console.log('err : ' + err);
+  //     }
+  //   };
+  //   getQoutes();
+  // }, []);
+
+  console.log('test : ' + qoutes);
+
+  // return <HomeScreen />;
+  return (
+    <View>
+      <Text style={styles.titleSize}>
+        {/* {qoutes?[Math.floor(Math.random() * qoutes.length)]} */}
+        {qoutes?.contents}
+      </Text>
+      <Text style={styles.authorSize}>{'-' + qoutes?.author + '-'}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -27,8 +86,16 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
+  titleSize: {
+    fontSize: 50,
+    textAlign: 'center',
+  },
+  authorSize: {
+    fontSize: 25,
+    textAlign: 'right',
+  },
   sectionTitle: {
-    fontSize: 24,
+    fontSize: 200,
     fontWeight: '600',
   },
   sectionDescription: {
